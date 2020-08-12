5a3c1b8cec66d330841e77eeba6769fcfcce83d2
etwillbefine <tim.gatzemeier@gmx.de>
2020-08-12 19:56:30 +0200

    correctly configure k8s provider

--- provider.tf
+++ provider.tf
@@ -1,12 +1,13 @@
 terraform {
   required_version = ">= 0.12.1"
-
-  required_providers {
-    kubernetes = "~> 1.11"
-  }
 }
 
 provider "aws" {
   version = "~> 2.50"
   region  = var.aws_region
+}
+
+provider "kubernetes" {
+  version          = "~> 1.11"
+  load_config_file = false
 }
