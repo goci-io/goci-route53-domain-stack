e36fb40754c55de2aa774a0f81861de9a2953ecb
Tim Gatzemeier <etwillbefine@users.noreply.github.com>
2020-08-22 14:16:33 +0200

    use name prefix for issuer resources

--- certs.tf
+++ certs.tf
@@ -5,7 +5,7 @@ module "acme" {
 
 resource "kubernetes_secret" "tls_ca_key" {
   metadata {
-    name      = format("%s-route53dns-ca", var.namespace)
+    name      = format("%s-ca", var.name)
     namespace = var.k8s_namespace
   }
 
@@ -26,7 +26,7 @@ resource "null_resource" "apply_issuer" {
       hosted_zone_id     = module.zone.zone_id
       email              = var.certificate_email
       aws_region         = var.aws_region
-      name               = var.namespace
+      name               = var.name
     })
   }
 }
